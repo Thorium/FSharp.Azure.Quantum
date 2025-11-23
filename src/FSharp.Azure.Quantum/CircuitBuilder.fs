cde0788eacd40075740df3edc4d0e5c5fc7a9ed1
Tuomas Hietanen <thorium@iki.fi>
2025-11-23 23:28:46 +0000

    TDD GREEN Cycle #2: Implement circuit construction API

--- src/FSharp.Azure.Quantum/CircuitBuilder.fs
+++ src/FSharp.Azure.Quantum/CircuitBuilder.fs
@@ -31,3 +31,21 @@ module CircuitBuilder =
     /// Gets the number of qubits in a circuit
     let qubitCount (circuit: Circuit) : int =
         circuit.QubitCount
+
+    /// Adds a single gate to the end of a circuit
+    let addGate (gate: Gate) (circuit: Circuit) : Circuit =
+        { circuit with Gates = circuit.Gates @ [gate] }
+
+    /// Adds multiple gates to the end of a circuit
+    let addGates (gates: Gate list) (circuit: Circuit) : Circuit =
+        { circuit with Gates = circuit.Gates @ gates }
+
+    /// Composes two circuits by appending the gates of the second to the first
+    let compose (circuit1: Circuit) (circuit2: Circuit) : Circuit =
+        if circuit1.QubitCount <> circuit2.QubitCount then
+            failwith "Cannot compose circuits with different qubit counts"
+        { circuit1 with Gates = circuit1.Gates @ circuit2.Gates }
+
+    /// Gets all gates from a circuit in order
+    let getGates (circuit: Circuit) : Gate list =
+        circuit.Gates
